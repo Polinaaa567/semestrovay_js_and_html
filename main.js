1540bf10552e9119951fc33d9dfd8abd82522d7e
Kolesnik Polina <kolesnik.03.polina@gmail.com>
2023-06-01 16:51:25 +0700

    4

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 
-let name = localStorage.getItem('username');
+let username = localStorage.getItem('username');
 
-document.getElementById('username').innerHTML = name;
+document.getElementById('username').innerHTML = username;
+
+let buttonAll = document.createElement('button');
+
+buttonAll.id = 'buttonAll'
+
+buttonAll.onclick = function() {
+    // необходимо здесь подключать табличку с покемонами и чекбоксами
+}
+
+buttonAll.textContent='All';
+
+document.body.appendChild(buttonAll)
--- welcome.js
+++ welcome.js
@@ -1,8 +1,33 @@
-function Name() {
-    let name = document.getElementById('name').value;
+let form = document.createElement('form');
+
+let askLabel = document.createElement('label');
+askLabel.innerText = "Введите своё имя"
+askLabel.htmlFor = 'name'
+
+let nameInput = document.createElement('input');
+nameInput.type = 'text';
+nameInput.id = 'name';
+nameInput.name = 'name';
+nameInput.placeholder = "Имя"
+
+let buttonNext = document.createElement('button');
+buttonNext.type = 'submit'
+buttonNext.innerText = 'Далее'
+
+buttonNext.addEventListener('click', (event) => {
+    event.preventDefault();
+    let name = nameInput.value;
     if(name === '') {
         alert('Введите имя!!!!');
     } else {
         localStorage.setItem('username', name);
+        window.location.replace('main.html')
+        
     }
-}
+   
+});
+
+form.append(askLabel, nameInput, buttonNext);
+
+document.body.append(form);
+
